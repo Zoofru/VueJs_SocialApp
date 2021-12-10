0ab4762bb3faad678419eac0a647464a1a32eba7
Zoofru <masonbush752@gmail.com>
2021-12-09 22:52:18 -0600

    Profile work. sidebar, header. backend work

---
+++ src/components/SidebarProfile.vue
@@ -0,0 +1,107 @@
+<script>
+export default {
+    name: 'SidebarProfile',
+    computed: {
+        user() {
+            return this.$store.getters.user
+        }
+    },
+    data() {
+        return {
+            sidebarTabs: ['Lasting Marks', 'Tags Achieved',  'Liked', 'Sparks', 'Respect']
+        }
+    }
+}
+</script>
+
+<template>
+    <div id='root'>
+        <button id='edit-profile'>Edit profile</button>
+        <div id='bar'>
+            <div id='tab' v-for="(tab, index) in this.sidebarTabs" :key=index>
+                <!-- <p id='title'>{{tab}}</p> -->
+                <p id='title' class='lasting-marks' v-if="tab == 'Lasting Marks'">{{tab}}</p>
+                <p id='title' v-else-if="tab == 'Tags Achieved'">{{tab}}</p>
+                <div id='tab-content' v-else-if="tab == 'Liked'">
+                    <p id='title'>{{tab}}</p>
+                    <p id='num'>{{this.user.receivedLikes}}</p>
+                </div>
+                <div v-else-if="tab == 'Sparks'">
+                    <p id='title'>{{tab}}</p>
+                    <p id='num'>{{this.user.sparksAchieved}}</p>
+                </div>
+                <div v-else-if="tab == 'Respect'">
+                    <p id='title'>{{tab}}</p>
+                    <p id='num'>{{this.user.respectEarned}}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+#root {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    border-radius: 5px;
+    height: 100%;
+}
+
+#tab-content {
+    height: 5%;
+}
+
+#title.lasting-marks {
+    margin-top: 2vh;
+}
+
+#edit-profile {
+    background-color: var(--main-color-blue);
+    color: white;
+    border: none;
+    border-radius: 5px;
+    width: 75%;
+    height: 4vh;
+    margin: 7% 0;
+}
+
+#edit-profile:hover {
+    background-color: white;
+    color: var(--main-color-blue);
+    cursor: pointer;
+    border: 1px solid var(--main-color-blue);
+}
+
+#num {
+    color: var(--main-color-blue);
+    font-size: 22px;
+    margin: 0;
+}
+
+#bar {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    background-color: white;
+    width: 90%;
+    height: 100%;
+}
+
+#tab {
+    margin: 3%;
+    height: 30%;
+}
+
+#title {
+    margin: 10px;
+    font-size: large;
+    margin-bottom: 0;
+    margin-left: 0;
+}
+
+
+</style>
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,9 +1,16 @@
 <script>
 import Nav from '../components/Nav.vue'
+import SidebarProfile from '../components/SidebarProfile.vue'
 export default {
     name: 'Profile',
     components: {
-        Nav
+        Nav,
+        SidebarProfile
+    },
+    computed: {
+        user() {
+            return this.$store.getters.user
+        }
     },
 }
 </script>
@@ -11,9 +18,86 @@ export default {
 <template>
     <div>
         <Nav />
+        <div id='profile-content-root'>
+            <div id='content'>
+                <div id='profile'>
+                    <div id='profile-content' v-if="this.user !== null">
+                        <img id='profile-avatar' v-bind:src='this.user.avatar' />
+                        <div id='header-content'>
+                            <h1 id='username'>{{this.user.username}}</h1>
+                            <h4 id='fullname'>{{this.user.name}}</h4>
+                            <p id='bio'>{{this.user.about}}</p>
+                        </div>
+                    </div>
+                </div>
+                <div id='sidebar'>
+                    <SidebarProfile />
+                </div>
+            </div>
+        </div>
     </div>    
 </template>
 
 <style scoped>
+#sidebar {
+    width: 20%;
+    height: 100%;
+}
 
+#profile-avatar {
+    width: 20%;
+    height: 90%;
+    border: 2px solid black;
+    margin-top: 1%;
+    margin-left: 1%;
+}
+
+#content {
+    display: flex;
+    justify-content: center;
+    width: 60%;
+    background-color: rgba(0, 0, 0, .1);
+    box-shadow: 0px 0px 3px 1px black;
+}
+
+#profile-content-root {
+    display: flex;
+    justify-content: center;
+    margin-top: 2%;
+}
+
+#profile {
+    width: 80%;
+    display: flex;
+    align-items: center;
+    flex-direction: row;
+    height: 21vh;
+}
+
+#profile-content {
+    background-color: white;
+    width: 100%;
+    display: flex;
+    height: 21vh;
+    border-radius: 5px;
+}
+
+#header-content {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin-left: 5%;
+}
+
+#bio {
+    margin-top: 15%;
+}
+
+#username, #fullname {
+    margin: 0;
+}
+
+#fullname {
+    color: gray;
+}
 </style>
